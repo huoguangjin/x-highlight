9f2050a4d1b9ca3b6b0003455fbf7c29ebae2fdc
huoguangjin <352886006@qq.com>
2017-10-22 18:40:27 +0800

    [config] move plugin files to /dist

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './js/preview.js'
   ],
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devServer: {
